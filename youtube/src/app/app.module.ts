f4538d5e7a99ba7575e2cbbfbaec379ec6f9be52
justasti <justas.tirevicius@gmail.com>
2022-08-07 14:07:58 +0300

    init: start of project

---
+++ youtube/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import AppComponent from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchBarComponent } from './header/search-bar/search-bar.component';
+import { UserInfoComponent } from './header/user-info/user-info.component';
+import { SortToolbarComponent } from './sort-toolbar/sort-toolbar.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
+import { SearchItemComponent } from './search-results/search-item/search-item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SearchBarComponent,
+    UserInfoComponent,
+    SortToolbarComponent,
+    SearchResultsComponent,
+    SearchItemComponent,
+  ],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export default class AppModule {}
---
+++ youtube/src/app/sort-toolbar/sort-toolbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sort-toolbar',
+  templateUrl: './sort-toolbar.component.html',
+  styleUrls: ['./sort-toolbar.component.scss']
+})
+export class SortToolbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
